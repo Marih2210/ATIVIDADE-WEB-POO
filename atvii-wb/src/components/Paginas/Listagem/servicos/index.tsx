cae1ef71983fcc82ee8333ff75da638bd7ed0130
Mariana Veloso de Souza <100208153+Marih2210@users.noreply.github.com>
2023-05-18 11:07:56 -0300

    atvii-wb

---
+++ atvii-wb/src/components/Paginas/Listagem/servicos/index.tsx
@@ -0,0 +1,83 @@
+/* eslint-disable react/jsx-pascal-case */
+import { PencilSimple, Eraser, Eye } from 'phosphor-react';
+import { Component } from 'react';
+import { Button, Table } from 'react-bootstrap';
+import BarraNav from '../../../NavBar';
+
+export class Servicos extends Component {
+    render(){
+
+        return (
+            <section>
+            <header>
+                <BarraNav/>
+            </header>
+            <main>
+                <h1>Serviços</h1>
+                <div className="tables">
+                    <Table striped bordered hover variant="light">
+                        <thead>
+                            <tr>
+                            <th>Id</th>
+                            <th>Serviço</th>
+                            <th>Preço</th>
+                            <th>Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Cortar cabelo</td>
+                                <td>R$30</td>
+                                <td>
+                                    <div className="icons">
+                                        <a href="/editar_servico/1"><PencilSimple size={35} color="#198754"/></a>
+                                        <a href="/servicos/1"><Eye size={35} color="#0DCAF0"/></a>
+                                        <a href="##"><Eraser size={35} color="#DC3545"/></a>           
+                                    </div>
+                                    <div className="Column">
+                                        <Button variant="outline-success" href='/editar_servico/1'>Editar</Button>{' '}
+                                        <Button variant="outline-danger">Excluir</Button>{' '}
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Limpeza de pele</td>
+                                <td>R$100</td>
+                                <td>
+                                    <div className="icons">
+                                        <a href="##"><PencilSimple size={35} color="#198754"/></a>
+                                        <a href="##"><Eye size={35} color="#0DCAF0"/></a>
+                                        <a href="##"><Eraser size={35} color="#DC3545"/></a>           
+                                    </div>
+                                    <div className="Column">
+                                        <Button variant="outline-success" href='##'>Editar</Button>{' '}
+                                        <Button variant="outline-danger">Excluir</Button>{' '}
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Massagem</td>
+                                <td>R$80</td>
+                                <td>
+                                    <div className="icons">
+                                        <a href="##"><PencilSimple size={35} color="#198754"/></a>
+                                        <a href="##"><Eye size={35} color="#0DCAF0"/></a>
+                                        <a href="##"><Eraser size={35} color="#DC3545"/></a>           
+                                    </div>
+                                    <div className="Column">
+                                        <Button variant="outline-success" href='##'>Editar</Button>{' '}
+                                        <Button variant="outline-danger">Excluir</Button>{' '}
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+            </main>
+        </section>
+    );
+}
+}
